c1baa18ab9151b1cba2ef5a5b773803217ad495c
rodrigo <rodrigo010497@gmail.com>
2020-12-03 04:04:21 +0000

    added base login functionality

---
+++ Login/src/java/pages/LogIn.java
@@ -0,0 +1,107 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pages;
+
+import com.UserServLet;
+import java.io.IOException;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import model.Jdbc;
+
+/**
+ *
+ * @author me-aydin
+ */
+public class LogIn extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        
+        HttpSession session = request.getSession(false);
+        
+        String [] query = new String[2];
+        query[0] = (String)request.getParameter("username");
+        query[1] = (String)request.getParameter("password");
+        String qry = "SELECT ROLE FROM USERS WHERE UNAME = '" + query[0] +"' AND PASSWD = '"+ query[1] +"'";
+        
+       
+        //String insert = "INSERT INTO `Users` (`username`, `password`) VALUES ('";
+      
+        Jdbc jdbc = (Jdbc)session.getAttribute("dbbean");
+        
+        if (jdbc == null)
+            request.getRequestDispatcher("/WEB-INF/conErr.jsp").forward(request, response);
+        
+        String msg = "wrong username or password";
+         try {
+                msg = jdbc.retrieve(qry);
+            } catch (SQLException ex) {
+                Logger.getLogger(UserServLet.class.getName()).log(Level.SEVERE, null, ex);
+            }
+         
+        request.setAttribute("query", msg);
+        request.setAttribute("message", "Welcome " + msg.toString() + query[0].toString());
+        request.getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
+        
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
